5b6f404f4c41d21e83ccfbf3102415a1aea76156
Kennedy Ekhator <kenekha@gmail.com>
2024-07-30 15:21:18 +0100

    create a random fact app using an api

---
+++ random-fact-app/random-fact.js
@@ -0,0 +1,34 @@
+const button = document.getElementById("btn");
+const fact = document.getElementById("fact");
+const apiKey = "B59DY1hTgKBCOsEKoIx73Q==y9yfhtDfvZfk1pu5";
+
+const url = "https://api.api-ninjas.com/v1/facts"
+
+const options = {
+    method: "GET",
+    headers: {
+         'X-Api-Key': apiKey},
+}
+
+ async function getFact() {
+    try {
+        fact.innerHTML = "Updating..."
+        button.disabled = true;
+        button.innerHTML = "loading"
+        const response  = await fetch(url, options);
+    const data = await response.json();
+    
+    button.disabled = false;
+    button.innerHTML = "TELL ME A FACT"
+    
+
+    fact.innerHTML = data[0].fact
+    } catch (error) {
+        fact.innerHTML = "something is wrong, please try again";
+        fact.style.color = "red"
+    }
+    
+}
+
+
+button.addEventListener("click", getFact)
